5f5b119655750ea7c9a1afa016f618336e0a6740
christiano-santos <christiano_94oliveira@hotmail.com>
2022-05-20 22:24:18 -0300

    desativado eslint para o arquivo

--- src/components/ResultGames/TableDupla/index.tsx
+++ src/components/ResultGames/TableDupla/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import { Table, Td, Th } from "../../TitleGame/styled";
 import { DivRes, Sorteio } from "../Dupla/style";
